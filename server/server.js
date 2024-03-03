772cdc44d3aaa4688ce49ccddf060b5de5fab583
Suryav06 <73733088+Suryav06@users.noreply.github.com>
2024-03-03 23:56:36 +0530

    change

--- server/server.js
+++ server/server.js
@@ -8,10 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({
-    origin: 'http://localhost:3000', 
-    credentials: true 
-}));
+app.use(cors());
 
 const pool = new pg.Pool({
     user: 'ul5a89srpjerd2ka3cek',
